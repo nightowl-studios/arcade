1b76bb71c34cbed78b1720877dc967415bc0cb0f
Byron Seto <byron.seto@circlecvi.com>
2020-06-24 03:18:55 -0600

    adds tests for registry register function

--- backend/websocket/identifier/identifier.go
+++ backend/websocket/identifier/identifier.go
@@ -5,10 +5,18 @@ package identifier
 // Client struct should contain all information about the client connected via
 // websocket
 type Client struct {
-	ClientUUID  string // ClientUUID is a completely unique identifier
-	HubName     string // HubName is the name of the hub they are connected to
-	DisplayName string // DisplayName is the name the user chose
+	ClientUUID  ClientUUIDStruct // ClientUUID is a completely unique identifier
+	HubName     HubNameStruct    // HubName is the name of the hub they are connected to
+	DisplayName string           // DisplayName is the name the user chose
 
 	// this Client struct may also include things like a jwt token or something
 	// later so that we can code in some reconnect functionality
+}
+
+type ClientUUIDStruct struct {
+	UUID string
+}
+
+type HubNameStruct struct {
+	HubName string
 }
--- backend/websocket/registry/registry.go
+++ backend/websocket/registry/registry.go
@@ -11,27 +11,23 @@ type Registry interface {
 	Register(send chan []byte, clientID identifier.Client)
 	Unregister(clientID identifier.Client)
 
-	//SendToSameHub(clientID identifier.Client, message []byte)
-	//SendToCaller(clientID identifier.Client, message []byte)
+	SendToSameHub(clientID identifier.Client, message []byte)
+	SendToCaller(clientID identifier.Client, message []byte)
 }
 
-// RegistryProvider will
+// RegistryProvider will provide the actual registry functionality
 type RegistryProvider struct {
 	lookupLock sync.RWMutex
-	//lookupMap map[id.Client.HubName]map[id.Client.ClientUUID](chan []byte)
+	lookupMap  map[identifier.HubNameStruct]map[identifier.ClientUUIDStruct](chan []byte)
 }
 
 func GetRegistryProvider() *RegistryProvider {
-	return &RegistryProvider{}
+	return &RegistryProvider{
+		lookupMap: make(
+			map[identifier.HubNameStruct]map[identifier.ClientUUIDStruct](chan []byte),
+		),
+	}
 }
-
-//func GetRegistryProvider() RegistryProvider {
-//return RegistryProvider {
-//lookupMap: make(
-//map[id.Client.HubName]map[id.Client.ClientUUID](chan []byte),
-//),
-//}
-//}
 
 func (r *RegistryProvider) Register(
 	send chan []byte,
