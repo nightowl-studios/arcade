d450c62342eb8152958a4a1a92bed8a93320ebf3
Gordon Cheung <gordoncheung2@gmail.com>
2020-06-26 22:56:15 -0600

    Add bootstrap-vue and sass loader to package.json

--- frontend-2/src/main.js
+++ frontend-2/src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+import './index.scss'
 
 Vue.config.productionTip = false
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 new Vue({
   render: h => h(App),
