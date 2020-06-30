fc0bef20185b90b8be798912d8f84d07506cd6ad
Zachary <mhkoo@ucalgary.ca>
2020-06-29 21:00:03 -0600

    actually adding the files this time

---
+++ backend/game/scribble/handler/checkAnswer/checkAnswer.go
@@ -0,0 +1,85 @@
+package checkAnswer
+
+import (
+	"encoding/json"
+
+	"github.com/bseto/arcade/backend/game"
+	"github.com/bseto/arcade/backend/log"
+	"github.com/bseto/arcade/backend/websocket/identifier"
+	"github.com/bseto/arcade/backend/websocket/registry"
+)
+
+// Trying to make a function that handles user's answers and compare to the
+// real answer.
+const (
+	name string = "guess"
+)
+
+type IsCorrect struct{}
+
+type GuessMessage struct {
+	Answer string `json:"answer"`
+	Guess  string `json:"userGuess"`
+}
+
+type GuessResult struct {
+	Result string `json:"result"`
+}
+
+func Get() *IsCorrect {
+	return &IsCorrect{}
+}
+
+func (i *IsCorrect) HandleInteraction(
+	message json.RawMessage,
+	caller identifier.Client,
+	registry registry.Registry,
+) {
+	var isCorrect bool
+	isCorrect = false
+	var msg GuessMessage
+	err := json.Unmarshal(message, &msg)
+	if err != nil {
+		log.Errorf("unable to unmarshal message: %v", err)
+		return
+	}
+
+	if msg.Guess == msg.Answer {
+		isCorrect = true
+	}
+
+	var result string
+
+	if isCorrect == true {
+		result = "Your guess is correct!"
+	} else {
+		result = "Your guess is incorrect."
+	}
+
+	payload := GuessResult{
+		Result: result,
+	}
+
+	payloadbytes, err := json.Marshal(payload)
+	if err != nil {
+		log.Errorf("unable to marshal message: %v", err)
+		return
+	}
+
+	response := game.Message{
+		API:     name,
+		Payload: payloadbytes,
+	}
+
+	responseMessage, err := json.Marshal(response)
+	if err != nil {
+		log.Errorf("unable to marshal message: %v", err)
+		return
+	}
+
+	registry.SendToCaller(caller, responseMessage)
+}
+
+func (i *IsCorrect) Name() string {
+	return name
+}
