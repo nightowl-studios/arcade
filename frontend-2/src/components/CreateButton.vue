e12354095e5f54178c1617c6096465729cbd9741
Gordon Cheung <gordoncheung2@gmail.com>
2020-06-29 19:32:41 -0600

    import axios

--- frontend-2/src/components/CreateButton.vue
+++ frontend-2/src/components/CreateButton.vue
@@ -5,13 +5,15 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: "CreateButton",
   methods: {
     createRoom: function() {
       console.log("Creating room...")
       // TODO make api request to backend
-      let apiUrl = "";
+      let apiUrl = "http://localhost:8080/hub";
       axios
         .get(apiUrl)
         .then(response => (this.$emit("onCreateRoom", response)));
