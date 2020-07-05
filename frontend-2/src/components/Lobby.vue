786da24cb8372a9a5589c567669937d2d9d2e972
Gordon Cheung <gordoncheung2@gmail.com>
2020-07-04 23:01:43 -0600

    Extract lobby component

---
+++ frontend-2/src/components/Lobby.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="lobby">
+    <h1>Lobby</h1>
+    <div v-for="player in players" :key="player">
+        <Player :name="player.name" :id="player.id" />
+    </div>
+</div>
+</template>
+
+<script>
+import Player from './Player.vue'
+
+export default {
+  name: 'Lobby',
+  components: {
+    Player
+  },
+  data: function() {
+    return {
+      players: [
+        { name: "Gordon", id: "ID12345"},
+        { name: "Byron", id: "ID12346"},
+        { name: "Zach", id: "ID12347" },
+        { name: "Sam", id: "ID12348" }
+      ],
+    }
+  }
+}
+</script>
