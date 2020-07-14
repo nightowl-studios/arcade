730fa541bd48cc2426d26c030a9eac9ffa503ac1
Mario Minardi <mminardi@shaw.ca>
2020-07-13 22:58:29 -0600

    Forward draw info to same hub except caller

--- backend/game/scribble/handler/draw/draw.go
+++ backend/game/scribble/handler/draw/draw.go
@@ -2,8 +2,9 @@ package draw
 
 import (
 	"encoding/json"
-	"time"
 
+	"github.com/bseto/arcade/backend/game"
+	"github.com/bseto/arcade/backend/log"
 	"github.com/bseto/arcade/backend/websocket/identifier"
 	"github.com/bseto/arcade/backend/websocket/registry"
 )
@@ -13,11 +14,30 @@ const (
 )
 
 type Handler struct {
-	//chatHistoryLock sync.RWMutex
-	//chatHistory     ChatHistory
 }
 
-type ChatTime time.Time
+type lineCap string
+
+const (
+	Round lineCap = "round"
+)
+
+type BrushStyle struct {
+	BrushSize  int32  `json:"brushSize"`
+	BrushColor string `json:"brushColor"`
+}
+
+type DrawPosition struct {
+	X int32 `json:"x"`
+	Y int32 `json:"y"`
+}
+
+type DrawMessage struct {
+	From       DrawPosition `json:"from"`
+	To         DrawPosition `json:"to"`
+	BrushStyle BrushStyle   `json:"brushStyle"`
+	LineCap    lineCap      `json:"lineCap"`
+}
 
 func Get() *Handler {
 	return &Handler{}
@@ -30,6 +50,30 @@ func (h *Handler) HandleInteraction(
 	caller identifier.Client,
 	registry registry.Registry,
 ) {
+	var msg DrawMessage
+	err := json.Unmarshal(message, &msg)
+	if err != nil {
+		log.Errorf("unable to unmarshal message: %v", err)
+		return
+	}
+
+	h.forwardDrawMessage(msg, caller, registry)
+
+	return
+}
+
+func (h *Handler) forwardDrawMessage(
+	drawMessage DrawMessage,
+	clientID identifier.Client,
+	reg registry.Registry) {
+
+	drawBytes, err := game.MessageBuild(name, drawMessage)
+	if err != nil {
+		log.Errorf("unable to build message: %v", err)
+		return
+	}
+
+	reg.SendToSameHubExceptCaller(clientID, drawBytes)
 }
 
 func (h *Handler) NewClient(
