597509dc52fec432093da53fe93e5552e4214754
Zachary <mhkoo@ucalgary.ca>
2020-07-16 04:14:15 -0600

    updating first version of word factory

--- backend/util/wordfactory/wordfactory.go
+++ backend/util/wordfactory/wordfactory.go
@@ -7,7 +7,13 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
+
+func randInt(min int, max int) int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return min + rand.Intn(max-min)
+}
 
 func WordGenerator() string {
 	b, err := ioutil.ReadFile("wordbank.txt")
@@ -20,28 +26,59 @@ func WordGenerator() string {
 	wordList := []string{allWords}
 
 	words := strings.Split(wordList[0], "\r\n")
-	pickWord := rand.Intn(len(words))
+	pickWord := randInt(0, len(wordList))
 	return words[pickWord]
 }
 
-func WordGenerator2() string {
+func WordGenerator2() (string, error) {
 	file, err := os.Open("wordbank.txt")
+	defer file.Close()
+	if err != nil {
+		fmt.Println("file could not be read", err)
+		return "", err
+	}
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Println("file info cannot be read", err)
+		return "", err
+	}
+
+	var fileSize int = int(fileInfo.Size())
+	randomLocation := randInt(0, fileSize-10)
+	for randomLocation <= 10 {
+		randomLocation = randInt(0, fileSize-10)
+	}
+
+	reader := bufio.NewReader(file)
+
+	reader.Discard(randomLocation)
+
+	data, _, _ := reader.ReadLine()
+	data, _, err = reader.ReadLine()
+	if err != nil {
+		fmt.Println("Cannot read next line", err)
+		return "", err
+	}
+
+	return (string(data)), nil
+}
+
+func testWordGenerator2(testrand int) (int, error) {
+	file, err := os.Open("wordbank.txt")
+	defer file.Close()
 	if err != nil {
 		fmt.Println("file could not be read", err)
 	}
+
 	fileInfo, err := file.Stat()
 	if err != nil {
 		fmt.Println("file info cannot be read", err)
 	}
 
 	var fileSize int = int(fileInfo.Size())
-	randomLocation := rand.Intn(fileSize)
-	reader := bufio.NewReader(file)
-
-	reader.Discard(randomLocation)
-
-	data, _, _ := reader.ReadLine()
-	data, _, _ = reader.ReadLine()
-	file.Close()
-	return (string(data))
+	for testrand <= 10 {
+		testrand = randInt(0, fileSize-10)
+	}
+	return testrand, nil
 }
--- backend/util/wordfactory/wordfactory_test.go
+++ backend/util/wordfactory/wordfactory_test.go
@@ -6,8 +6,13 @@ import (
 	"math/rand"
 	"os"
 	"testing"
+	"time"
 )
 
+func randInt2(min int, max int) int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return min + rand.Intn(max-min)
+}
 func BenchmarkWordGenerator1(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		WordGenerator()
@@ -20,6 +25,11 @@ func BenchmarkWordGenerator2(b *testing.B) {
 	}
 }
 
+// func TestWordFactoryFunction(t *testing.T) {
+// 	ans, _ := testWordGenerator2(0)
+// 	t.Error(ans)
+// }
+
 func TestYourFunction(t *testing.T) {
 	file, err := os.Open("wordbank.txt")
 	if err != nil {
@@ -31,13 +41,16 @@ func TestYourFunction(t *testing.T) {
 	}
 
 	var fileSize int = int(fileInfo.Size())
-	randomLocation := rand.Intn(fileSize)
-	reader := bufio.NewReader(file)
+	randomLocation := randInt2(0, fileSize-10)
+	for randomLocation <= 10 {
+		randomLocation = randInt2(0, fileSize-10)
+	}
 
+	reader := bufio.NewReader(file)
 	reader.Discard(randomLocation)
 
-	data, _, _ := reader.ReadLine()
-	data, _, _ = reader.ReadLine()
-	fmt.Println(string(data))
+	// data, _, _ := reader.ReadLine()
+	// data, _, _ = reader.ReadLine()
+	// t.Error(string(data))
 	file.Close()
 }
