43f8974ed64a2bfe3cfdbb7521dd801e8de79096
Gordon Cheung <gordoncheung2@gmail.com>
2020-07-20 23:35:28 -0600

    Update layout of scribble page

---
+++ frontend/src/modules/scribble/components/PlayerList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="player-list">
+        <h2>Players</h2>
+        <div v-for="player in players" :key="player">{{ player.nickname }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PlayerList",
+    props: {
+        players: Array,
+    },
+};
+</script>
+
+<style scoped>
+#player-list {
+    flex: 1;
+    background-color: lightblue;
+}
+</style>
