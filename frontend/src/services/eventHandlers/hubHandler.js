9c5f250598c0c5909a9a6d55b7ddf15268bd5043
Gordon Cheung <gordoncheung2@gmail.com>
2020-07-22 22:55:34 -0600

    Rename player name to nickname

--- frontend/src/services/eventHandlers/hubHandler.js
+++ frontend/src/services/eventHandlers/hubHandler.js
@@ -1,10 +1,12 @@
+import Player from "@/modules/common/entities/player";
 import { store } from "@/store";
 
 // Event handler for Hub API
 export default class HubHandler {
     handle(payload) {
         if (payload.connectedClients != null) {
-            store.commit("application/setPlayers", payload.connectedClients);
+            const players = payload.connectedClients.map(client => new Player(client.clientUUID.UUID, client.nickname, client.joinOrder));
+            store.commit("application/setPlayers", players);
         }
     }
 }
