325d0f0b88803fd8a202c45c29d224e8d5571411
Gordon Cheung <gordoncheung2@gmail.com>
2020-07-22 23:39:07 -0600

    Align header to middle of page'

---
+++ frontend/src/modules/scribble/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="header">{{ nickname }} is currently drawing...</div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    props: {
+        nickname: String,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+    text-align: center;
+}
+</style>
+
