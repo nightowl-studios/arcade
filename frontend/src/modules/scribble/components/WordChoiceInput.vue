d35a2dba0d77d31a7f6c1bd5aebd33d20a9828de
rickngo <rickngo@users.noreply.github.com>
2020-07-25 01:30:17 -0600

    Added WordChoice and WordChoiceInput components to scribble module (#228)
    Co-authored-by: Gordon Cheung <gordoncheung2@gmail.com>

---
+++ frontend/src/modules/scribble/components/WordChoiceInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="word-choice-input">
+        <b-row>
+            <b-col class="word-choice-input__col text-center">
+                <b-button class="scribble-button" variant="primary">{{
+                    words[0]
+                }}</b-button>
+            </b-col>
+            <b-col class="word-choice-input__col text-center">
+                <b-button class="scribble-button" variant="primary">{{
+                    words[1]
+                }}</b-button>
+            </b-col>
+            <b-col class="word-choice-input__col text-center">
+                <b-button class="scribble-button" variant="primary">{{
+                    words[2]
+                }}</b-button>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "WordChoice",
+    props: {
+        words: Array,
+    },
+};
+</script>
