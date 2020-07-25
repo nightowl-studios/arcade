4fb09f758929453c770035854bd1268558755ff7
Alan Lam <alanlam56@gmail.com>
2020-07-24 21:38:43 -0600

    -added GenerateWordList() in wordfactory that will generate an unique list of words. -added test for function generateWordList

--- backend/util/wordfactory/wordfactory_test.go
+++ backend/util/wordfactory/wordfactory_test.go
@@ -21,3 +21,26 @@ func BenchmarkWordGenerator2(b *testing.B) {
 		WordGenerator2(File)
 	}
 }
+
+func assertEqual(t *testing.T, a interface{}, b interface{}) {
+	if a != b {
+		t.Fatalf("%s != %s", a, b)
+	}
+}
+
+func TestGenerateWordList(t *testing.T) {
+	expectedBehaviour := []string{"hello", "world", "alain"}
+	actualBehaviour := generateWordList(3, annonymousFunc())
+	for i := 0; i < 3; i++ {
+		assertEqual(t, expectedBehaviour[i], actualBehaviour[i])
+	}
+}
+
+func annonymousFunc() func(string) string {
+	n := 0
+	return func(string) string {
+		testList := []string{"hello", "hello", "world", "world", "world", "alain"}
+		n++
+		return testList[n]
+	}
+}
