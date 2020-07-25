1d507b3244205e457d9088fcd0ca743aa79f9c66
Byron Seto <byron.seto@circlecvi.com>
2020-07-25 14:03:28 -0600

    Can now test what is coming into the mock calls and ensure they are correct

--- backend/game/scribble/handler/gamemaster/gamemaster_test.go
+++ backend/game/scribble/handler/gamemaster/gamemaster_test.go
@@ -2,9 +2,11 @@ package gamemaster
 
 import (
 	"encoding/json"
+	"reflect"
 	"testing"
 	"time"
 
+	"github.com/bseto/arcade/backend/game"
 	"github.com/bseto/arcade/backend/game/scribble/handler/gamemaster/util/point"
 	mockWf "github.com/bseto/arcade/backend/mocks/util/wordfactory"
 	mocks "github.com/bseto/arcade/backend/mocks/websocket/registry"
@@ -12,6 +14,9 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+// TestWaitForStart will test that the gamemaster starts in the WaitForStart state.
+// Afterwards, it'll check to make sure that it will not proceed to the next
+// state unless the correct message has been passed through.
 func TestWaitForStart(t *testing.T) {
 	var reg mocks.Registry
 	var wordFactory mockWf.WordFactory
@@ -63,9 +68,13 @@ func TestWaitForStart(t *testing.T) {
 	wordFactory.AssertExpectations(t)
 }
 
+// TestWordSelect tests the gamemaster starting at the WordSelect state.
+// This test will check that the messages sent to the front end initially are
+// correct.
 func TestWordSelect(t *testing.T) {
 	startingState := WordSelect
 	wordSelectTimer := time.Second * 10
+	wordChoices := []string{"a", "b", "c"}
 
 	var reg mocks.Registry
 	var wordFactory mockWf.WordFactory
@@ -89,13 +98,58 @@ func TestWordSelect(t *testing.T) {
 	}
 	gameMaster.NewClient(ID, &reg)
 
-	wordFactory.On("GenerateWordList", 3).Return([]string{"a", "b", "c"})
-	reg.On("SendToSameHubExceptCaller",
+	wordFactory.On("GenerateWordList", 3).Return(wordChoices)
+	reg.On(
+		"SendToSameHubExceptCaller",
 		mock.Anything,
-		mock.MatchedBy(func(msg Send) bool {
-			return false
+		mock.MatchedBy(func(msg []byte) bool {
+			var gameMsg game.Message
+			err := json.Unmarshal(msg, &gameMsg)
+			if err != nil {
+				t.Errorf("unable to unmarshal send: %v", err)
+				return false
+			}
+			if gameMsg.API != name {
+				t.Errorf("got: %v, expected: %v", gameMsg.API, name)
+			}
+			var send Send
+			err = json.Unmarshal(gameMsg.Payload, &send)
+			if err != nil {
+				t.Errorf("unable to unmarshal send: %v", err)
+			}
+			if send.GameMasterAPI != WordSelect {
+				t.Errorf("got: %v, expected: %v", send.GameMasterAPI, WordSelect)
+			}
+			if len(send.WordSelectSend.Choices) != 0 {
+				t.Errorf(
+					"there should be no word choices sent to other clients: len == %v",
+					len(send.WordSelectSend.Choices),
+				)
+			}
+
+			return true
 		}),
 	)
+	reg.On("SendToCaller", mock.Anything, mock.MatchedBy(func(msg []byte) bool {
+		var gameMsg game.Message
+		err := json.Unmarshal(msg, &gameMsg)
+		if err != nil {
+			t.Errorf("unable to unmarshal send: %v", err)
+			return false
+		}
+		if gameMsg.API != name {
+			t.Errorf("got: %v, expected: %v", gameMsg.API, name)
+		}
+		var send Send
+		err = json.Unmarshal(gameMsg.Payload, &send)
+		if err != nil {
+			t.Errorf("unable to unmarshal send: %v", err)
+		}
+		if !reflect.DeepEqual(send.WordSelectSend.Choices, wordChoices) {
+			t.Errorf("got: %v, expected: %v", send.WordSelectSend.Choices, wordChoices)
+		}
+		return true
+	}))
 
 	go gameMaster.run()
 
@@ -103,5 +157,4 @@ func TestWordSelect(t *testing.T) {
 
 	reg.AssertExpectations(t)
 	wordFactory.AssertExpectations(t)
-
 }
