3614899d2e3a152c8b6e68d50c013c1929fdeadf
alantylam <alanlam56@gmail.com>
2020-07-25 21:59:29 -0600

    added lobby id component to the scribble page (#234)

---
+++ frontend/src/modules/scribble/components/LobbyId.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="lobby-id">
+        <b-container fluid>
+            <b-row align-v="center" class="justify-content-md-center">
+                <b-col md="auto">Lobby ID: </b-col>
+                <b-col md="auto"
+                    ><input
+                        class="lobby-id__input"
+                        type="text"
+                        v-model="lobbyId"
+                        readonly
+                /></b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LobbyId",
+    data: function () {
+        return {
+            lobbyId: window.location.href.split("/").pop(),
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.lobby-id {
+    background-color: #e5f4fc;
+    border: 1px solid #a1c5d8;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    border-radius: 6px;
+    margin: 5px 5px 5px 5px;
+}
+
+.lobby-id__input {
+    color: black;
+    width: 60px;
+    text-align: center;
+    border-radius: 6px;
+}
+</style>
