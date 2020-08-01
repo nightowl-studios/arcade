374fca2721d17811a8540394641ae54f347bcd67
Gordon Cheung <gordoncheung2@gmail.com>
2020-07-31 19:16:38 -0600

    Display the selected word to the drawer (#255)
    Co-authored-by: Byron Seto <byronseto@hotmail.com>

--- frontend/src/modules/scribble/stores/scribblestore/index.js
+++ frontend/src/modules/scribble/stores/scribblestore/index.js
@@ -2,15 +2,22 @@ export default {
     namespaced: true,
     state: {
         gameState: null,
+        wordSelected: "",
     },
     getters: {
         getGameState: (state) => {
             return state.gameState;
         },
+        getWordSelected: (state) => {
+            return state.wordSelected;
+        },
     },
     mutations: {
         setGameState: (state, payload) => {
             state.gameState = payload;
+        },
+        setWordSelected: (state, payload) => {
+            state.wordSelected = payload;
         },
     },
     actions: {},
