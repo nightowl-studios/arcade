53d367a477c07f1d37aef6ad9a281cb2f574b864
Byron Seto <byronseto@hotmail.com>
2020-08-05 20:43:57 -0600

    have showResults send a message containing showResults (#274)
    Co-authored-by: Byron Seto <byron.seto@circlecvi.com>

--- backend/game/scribble/handler/gamemaster/showresults.go
+++ backend/game/scribble/handler/gamemaster/showresults.go
@@ -1,5 +1,27 @@
 package gamemaster
 
+import (
+	"github.com/bseto/arcade/backend/game"
+	"github.com/bseto/arcade/backend/log"
+)
+
+type ShowResultsSend struct {
+	ShowResults bool `json:"showResults"`
+}
+
 func (h *Handler) showResults() {
 
+	showResultsMsg := Send{
+		GameMasterAPI: ShowResults,
+		ShowResults: ShowResultsSend{
+			ShowResults: true,
+		},
+	}
+
+	scoreTimeBytes, err := game.MessageBuild("game", showResultsMsg)
+	if err != nil {
+		log.Fatalf("unable to marshal: %v", err)
+		return
+	}
+	h.reg.SendToSameHub(h.clientList.clients[0].ClientUUIDStruct, scoreTimeBytes)
 }
