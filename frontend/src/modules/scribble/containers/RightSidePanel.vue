bab59755b9c98499d75b0da4aef7c39a35bfe15f
Gordon Cheung <gordoncheung2@gmail.com>
2020-09-23 01:44:42 -0600

    Update timer location

--- frontend/src/modules/scribble/containers/RightSidePanel.vue
+++ frontend/src/modules/scribble/containers/RightSidePanel.vue
@@ -1,22 +1,15 @@
 <template>
     <div class="right-side-panel">
-        <b-row class="right-side-panel__header">
-            <RightSidePanelHeader />
-        </b-row>
-        <b-row class="right-side-panel__body">
-            <Chat />
-        </b-row>
+        <Chat />
     </div>
 </template>
 
 <script>
-import RightSidePanelHeader from "./RightSidePanelHeader.vue";
 import Chat from "../components/Chat.vue";
 
 export default {
     components: {
         Chat,
-        RightSidePanelHeader,
     },
 };
 </script>
