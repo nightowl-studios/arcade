02511b97c469f7f0a0fff53f79a552980cd5cc77
Gordon Cheung <gordoncheung2@gmail.com>
2020-09-23 01:30:39 -0600

    Add back in left and right side panel

--- frontend/src/modules/scribble/containers/LeftSide.vue
+++ frontend/src/modules/scribble/containers/LeftSide.vue
@@ -1,16 +1,28 @@
 <template>
     <div class="left-side-panel">
+        <PlayerList :players="players" />
     </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import PlayerList from "../components/PlayerList.vue";
+
 export default {
+    components: {
+        PlayerList,
+    },
+    computed: {
+        ...mapState("scribble", {
+            players: (state) => state.players,
+        }),
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 .left-side-panel {
     height: 100%;
-    background-color: blue;
+    border: 1px black solid;
 }
 </style>
--- frontend/src/modules/scribble/containers/RightSide.vue
+++ frontend/src/modules/scribble/containers/RightSide.vue
@@ -1,16 +1,29 @@
 <template>
     <div class="right-side-panel">
+        <b-row class="right-side-panel__header">
+            <RightSidePanelHeader />
+        </b-row>
+        <b-row class="right-side-panel__body">
+            <Chat />
+        </b-row>
     </div>
 </template>
 
 <script>
+import RightSidePanelHeader from "./RightSidePanelHeader.vue";
+import Chat from "../components/Chat.vue";
+
 export default {
+    components: {
+        Chat,
+        RightSidePanelHeader,
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 .right-side-panel {
     height: 100%;
-    background-color: gray;
+    border: 1px black solid;
 }
 </style>
