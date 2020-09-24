2d90bad638e890ab757cde55e99921c83c443589
Gordon Cheung <gordoncheung2@gmail.com>
2020-09-23 19:58:27 -0600

    User/gordon cheung/update ui (#315)
    * Fix chat and canvas update
    
    * Update round number on reconnect and next round
    
    * Fix round update and show scores on game over
    
    * Fix some bugs and when game is done send them back to home
    
    * Revamp UI to use 3 columns that are flexed
    
    * Add back canvas to center panel
    
    * Add back in left and right side panel
    
    * Remove legacy files
    
    * Update round number location
    
    * Update lobby id location
    
    * Update timer location

--- frontend/src/modules/scribble/containers/Header.vue
+++ frontend/src/modules/scribble/containers/Header.vue
@@ -9,18 +9,21 @@
             :word="gameState.word"
             :isGuessing="gameState.lockCanvas"
         />
+        <BaseTimerCircle style="float: right" size="sm"/>
     </div>
 </template>
 
 <script>
 import Word from "../components/Word.vue";
 import PlayerInfo from "../components/PlayerInfo.vue";
+import BaseTimerCircle from "@/modules/common/components/BaseTimerCircle.vue";
 
 import { mapState } from "vuex";
 export default {
     components: {
         PlayerInfo,
         Word,
+        BaseTimerCircle,
     },
     computed: {
         ...mapState("scribble", {
--- frontend/src/modules/scribble/containers/LeftSidePanel.vue
+++ frontend/src/modules/scribble/containers/LeftSidePanel.vue
@@ -1,16 +1,34 @@
 <template>
-    <div>
-        <PlayerList :players="players" />
-    </div>
+    <b-container class="left-side-panel">
+        <b-row>
+            <b-col>
+                <RoundNumber />
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+            <PlayerList :players="players" />
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+                <LobbyId />
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
 import { mapState } from "vuex";
 import PlayerList from "../components/PlayerList.vue";
+import RoundNumber from "../components/RoundNumber.vue";
+import LobbyId from "../components/LobbyId.vue";
 
 export default {
     components: {
         PlayerList,
+        RoundNumber,
+        LobbyId,
     },
     computed: {
         ...mapState("scribble", {
@@ -19,3 +37,10 @@ export default {
     },
 };
 </script>
+
+<style lang="scss" scoped>
+.left-side-panel {
+    height: 100%;
+    border: 1px black solid;
+}
+</style>
